7fe79ebd2fa1272a1e4bfb8fce6e83ac1f2c6141
Daniel Palmer <daniel@0x0f.com>
2018-07-13 05:03:58 +0900

    Define ctrl socket message format

--- ctrl.c
+++ ctrl.c
@@ -1,15 +1,38 @@
 #include <glib.h>
 #include <gio/gunixsocketaddress.h>
 #include "ctrl.h"
+#include "include/thingymcconfig/ctrl.h"
 
 #define CTRLSOCKPATH "/tmp/thingymcconfig.socket"
 
 static GSocketService* socketservice;
 
+static void ctrl_send_networkstate(GSocketConnection* connection) {
+	GByteArray* pktbuff = g_byte_array_new();
+
+	struct thingmcconfig_ctrl_field fields[] = { { .type =
+	THINGYMCCONFIG_FIELDTYPE_NETWORKSTATEUPDATE_SUPPLICANTSTATE }, { .type =
+	THINGYMCCONFIG_FIELDTYPE_NETWORKSTATEUPDATE_DHCPSTATE } };
+
+	struct thingymcconfig_ctrl_msgheader msghdr = { .type =
+	THINGYMCCONFIG_MSGTYPE_EVENT_NETWORKSTATEUPDATE, .numfields = G_N_ELEMENTS(
+			fields) };
+
+	g_byte_array_append(pktbuff, (void*) &msghdr, sizeof(msghdr));
+	g_byte_array_append(pktbuff, (void*) fields, sizeof(fields));
+
+	GOutputStream* os = g_io_stream_get_output_stream(G_IO_STREAM(connection));
+	g_output_stream_write(os, pktbuff->data, pktbuff->len, NULL, NULL);
+	g_byte_array_free(pktbuff, TRUE);
+}
+
 static gboolean ctrl_incomingcallback(GSocketService *service,
 		GSocketConnection *connection, GObject *source_object,
 		gpointer user_data) {
 	g_message("incoming control socket connection");
+
+	ctrl_send_networkstate(connection);
+
 	return FALSE;
 }
 
--- include/thingymcconfig/ctrl.h
+++ include/thingymcconfig/ctrl.h
@@ -1,8 +1,33 @@
 #ifndef THINGYMCCONFIG_CTRL_H_
 #define THINGYMCCONFIG_CTRL_H_
 
-struct thingymconfig_ctrl_msgheader {
-	size_t len;
+#define THINGYMCCONFIG_MSGTYPE_NULL                                   0
+#define THINGYMCCONFIG_MSGTYPE_EVENT_NETWORKSTATEUPDATE               1
+
+#define THINGYMCCONFIG_FIELDTYPE_NETWORKSTATEUPDATE_SUPPLICANTSTATE   1
+#define THINGYMCCONFIG_FIELDTYPE_NETWORKSTATEUPDATE_DHCPSTATE         2
+
+/*
+ * Header before each message with the type of message and
+ * the number of fields in the message.
+ */
+
+struct __attribute__((__packed__)) thingymcconfig_ctrl_msgheader {
+	unsigned char type;
+	unsigned char numfields;
+	// pad up to 32bits, might be used for something later
+	unsigned char pad0, pad1;
+};
+
+/*
+ * Opaque field in message. Type is namespaced by the type in the
+ * header. i.e. field type 0 in message type 0 is different to field type
+ * 0 in message type 1.
+ */
+
+struct __attribute__((__packed__)) thingymcconfig_ctrl_field {
+	unsigned char type;
+	unsigned char v0, v1, v2;
 };
 
 #endif
