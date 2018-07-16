5681ce942b86ae981f5004cac755562e4293db44
Daniel Palmer <daniel@0x0f.com>
2018-07-17 01:45:06 +0900

    Fix up passing of wpa_supplicant path

--- buildconfig.h
+++ buildconfig.h
@@ -12,14 +12,5 @@
 #endif
 
 #ifndef WPASUPPLICANT_BINARYPATH
-#warning "Assuming your wpa_supplicant binary is in /sbin/"
-#define WPASUPPLICANT_BINARYPATH "/sbin/wpa_supplicant"
-#endif
-
-#ifndef DHCPC_BINARYPATH
-#define DHCPC_BINARYPATH "/sbin/dhclient"
-#endif
-
-#ifndef DHCPD_BINARYPATH
-#define DHCPD_BINARYPATH "/usr/sbin/dhcpd"
+#error "Please define WPASUPPLICANT_BINARYPATH"
 #endif
