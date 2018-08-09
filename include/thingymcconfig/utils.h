53ad9cb7cec0e22a970c92741d5947ac15a8c582
Daniel Palmer <daniel@0x0f.com>
2018-08-09 23:05:11 +0900

    update gitignore. Add missing file.

---
+++ include/thingymcconfig/utils.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_THINGYMCCONFIG_UTILS_H_
+#define INCLUDE_THINGYMCCONFIG_UTILS_H_
+
+#include <glib.h>
+
+void thingymcconfig_utils_hexdump(guint8* payload, gsize len);
+
+#endif /* INCLUDE_THINGYMCCONFIG_UTILS_H_ */
